defbf1d4b5ddf96203acf508d61f5cd66d111308
BChaitanya3 <battelankachaitanya@gmail.com>
2026-02-10 17:57:30 +0530

    fix: index.html production issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/MY_PORTFOLIO/", // ✅ trailing slash REQUIRED
+  plugins: [react()],
+  base: "/",   // ✅ IMPORTANT (was /MY_PORTFOLIO/)
 });
